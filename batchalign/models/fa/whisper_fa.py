cb98f36f89e789b007d5737712fa081d049b697b
Houjun Liu <houjun@jemoka.com>
2023-12-14 13:04:27 -0800

    full whisper FA model

--- batchalign/models/fa/whisper_fa.py
+++ batchalign/models/fa/whisper_fa.py
@@ -7,76 +7,117 @@ from scipy.ndimage import median_filter
 from transformers.models.whisper.modeling_whisper import _dynamic_time_warping as dtw
 from transformers.models.whisper.modeling_whisper import _median_filter as median_filter
 
+from batchalign.models.asr.whisper import ASRAudioFile
+
 import numpy as np
 
-base = "openai/whisper-large-v2"
+import logging
+L = logging.getLogger("batchalign")
 
-transcript = "Hello . This is a test of Batchalign . I'm some body going to read wants told some random crap as I see on the screen . just to test batchline . the primary area for recording editing and me the world's gonna roll me arranging audio. MIDI and drummer regions divided into different track types . Press command slash for more info . test test . I don't know what to say . but um here's some retracing . so just in this for fun . um I like I like I like beans . beans are fun . thank you very much ."
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+TIME_PRECISION = 0.02
 
-audio = "./batchalign/tests/support/test.mp3"
+# inference engine
+class WhisperFAModel(object):
+    """An Forced Alignment engine built out of whisper
 
-# sample rate
-sample_rate = 16000
+    Parameters
+    ----------
+    model : str
+        The model path to load from.
+    target_sample_rate : optional, int
+        The sample rate to cast to. Defaults 16000 by Whisper.
 
-processor = WhisperProcessor.from_pretrained(base)
-model = WhisperForConditionalGeneration.from_pretrained(base)
-time_precision = 0.02
+    Example
+    -------
+    >>> engine = WhisperFAModel()
+    >>> file = engine.load("./data/myfile.wav")
+    >>> timestamps = engine(audio=file.chunk(0, 1500), text="this is my transcript") # FA
+    """
 
-###### 
+    def __init__(self, model="openai/whisper-large-v2", target_sample_rate=16000):
+        L.debug("Initializing whisper FA model...")
+        self.__model = WhisperForConditionalGeneration.from_pretrained(model)
+        L.debug("Done, initalizing processor and config...")
+        self.__processor = WhisperProcessor.from_pretrained(model)
+        L.debug("Whisper FA initialization done.")
 
-# function: load and resample audio
-audio_arr, rate = load(audio)
-duration = audio_arr.shape[-1]/rate
+        # save the target sample rate
+        self.sample_rate = target_sample_rate
 
-# resample if needed
-if rate != sample_rate:
-    audio_arr = T.Resample(rate, sample_rate)(audio_arr)
+    def load(self, f):
+        """Load an audio file for procesing.
 
-# transpose and mean
-resampled = torch.mean(audio_arr.transpose(0,1), dim=1)
+        Parameters
+        ----------
+        f : str
+            The audio .wav file name to process.
+        num_speakers : int
+            The number of speakers
 
-# input features
-features = processor(audio=resampled, text=transcript,
-                     sampling_rate=sample_rate, return_tensors='pt')
-tokens = features["labels"][0]
+        Returns
+        -------
+        Tuple[ASRAudioFile, List[dict]]
+            Return processed audio file and speaker segments.
+        """
 
-# perform inference to get cached qs
-with torch.inference_mode():
-    output = model(**features, output_attentions=True)
+        # function: load and resample audio
+        audio_arr, rate = load(f)
 
-# get decoder layer across attentions
-# which has shape layers x heads x output_tokens x input_frames
-cross_attentions = torch.cat(output.cross_attentions)
+        # resample if needed
+        if rate != self.sample_rate:
+            audio_arr = T.Resample(rate, self.sample_rate)(audio_arr)
 
-# get the attention of alignment heads we care about only
-weights = torch.stack([cross_attentions[l][h]
-                       for l, h in model.generation_config.alignment_heads])
+        # transpose and mean
+        resampled = torch.mean(audio_arr.transpose(0,1), dim=1)
 
-# normalize the attentino activations
-std, mean = torch.std_mean(weights, dim=-2, keepdim=True, unbiased=False)
-weights = (weights - mean) / std
+        # and return the audio file
+        return ASRAudioFile(f, resampled, self.sample_rate)
 
-# perform smoothing on attention activations + scale them
-weights = median_filter(weights, model.config.median_filter_width)
-matrix = weights.mean(axis=0)
+    def __call__(self, audio, text):
+        L.debug("Whisper Preprocessing...")
+        # input features
+        features = self.__processor(audio=audio, text=text,
+                                    sampling_rate=self.sample_rate,
+                                    return_tensors='pt')
+        tokens = features["labels"][0]
 
-# its dynamic time warping time
-text_idx, time_idx = dtw(-matrix)
-jumps = np.pad(np.diff(text_idx), (1, 0), constant_values=1).astype(bool)
-jump_times = time_idx[jumps] * time_precision
-timestamped_tokens = [(processor.decode(i),j) for i,j in zip(tokens, jump_times)]
-timestamped_tokens
+        L.debug("Running inference...")
+        # perform inference to get cached qs
+        with torch.inference_mode():
+            output = self.__model(**features, output_attentions=True)
 
+        L.debug("Collecting and normalizing activations...")
+        # get decoder layer across attentions
+        # which has shape layers x heads x output_tokens x input_frames
+        cross_attentions = torch.cat(output.cross_attentions)
 
+        # get the attention of alignment heads we care about only
+        weights = torch.stack([cross_attentions[l][h]
+                            for l, h in self.__model.generation_config.alignment_heads])
 
+        # normalize the attentino activations
+        std, mean = torch.std_mean(weights, dim=-2, keepdim=True, unbiased=False)
+        weights = (weights - mean) / std
 
-# [i.remove() for i in handles]
+        L.debug("Applying median filter...")
 
-# QKs
+        # perform smoothing on attention activations + scale them
+        weights = median_filter(weights, self.__model.config.median_filter_width)
+        matrix = weights.mean(axis=0)
 
-# weights = torch.cat(QKs)
-# [i for i in QKs if i == None]
+        L.debug("Applying dynamic time warping...")
 
-# # QKs[0]
+        # its dynamic time warping time
+        text_idx, time_idx = dtw(-matrix)
+        jumps = np.pad(np.diff(text_idx), (1, 0), constant_values=1).astype(bool)
+        jump_times = time_idx[jumps] * TIME_PRECISION
+        # align jumps against transcript and decode
+        timestamped_tokens = [(self.__processor.decode(i),j) for i,j in zip(tokens, jump_times)
+                            if i < 50200]
+        # TODO Jank most whisper models use 50000somethnig as the logistics tokens; but
+        # other models may not
 
-# # model = Whi
+        L.debug("Whisper FA done.")
+        # we now return the ruslts for later processing
+        return timestamped_tokens
