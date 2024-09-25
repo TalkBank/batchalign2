c8666eada12b5bea4a2dea20328a66471f74e924
Houjun Liu <houjun@jemoka.com>
2024-09-25 08:44:36 -0700

    [ci skip] resolve whisper to small for cantonese

--- batchalign/models/resolve.py
+++ batchalign/models/resolve.py
@@ -12,6 +12,7 @@ resolver = {
     },
     "whisper": {
         'eng': ("talkbank/CHATWhisper-en-large-v1", "openai/whisper-large-v2"),
+        'yue': ("alvanlii/whisper-small-cantonese", "alvanlii/whisper-small-cantonese"),
     }
 }
 
