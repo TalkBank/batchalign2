f49f183a319764950c25c08d830c0f8ee7e29a57
Houjun Liu <houjun@jemoka.com>
2025-01-07 09:23:37 -0800

    [ci skip] bump

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "click~=8.1",
         "matplotlib>=3.8.0,<4.0.0",
         "pyfiglet==1.0.2",
+        "setuptools",
         "soundfile~=0.12.0",
         "rich-click>=1.7.0",
         "typing-extensions",
